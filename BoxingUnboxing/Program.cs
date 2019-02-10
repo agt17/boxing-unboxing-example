b5442f3209d379aeae45ece5f1faae7c95c0eb7b
agtr <alfredgt94@hotmail.com>
2019-02-10 20:19:29 +0100

    boxing/unboxing example

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            // boxing
+            object o = i;
+
+            // unboxing
+            int sol = (int)o;
+
+            Console.WriteLine("Value of integer: {0}", i.ToString());
+            Console.WriteLine("Value of integer after boxing and unboxing: {0}", sol.ToString());
+        }
+    }
+}
